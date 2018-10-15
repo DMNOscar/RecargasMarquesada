29db2a577cae8f58a195333f726e8a6203f60da3
JavierSLX <jserranolule@gmail.com>
2018-10-15 18:50:34 -0500

    Comenzando con broadcast

---
+++ App/app/src/main/java/com/morpheus/ultrachip/BroadcastNetwork.java
@@ -0,0 +1,16 @@
+package com.morpheus.ultrachip;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+// https://stackoverflow.com/questions/17929086/android-net-wifi-wifi-state-changed-not-being-broadcast
+// https://stackoverflow.com/questions/36421930/connectivitymanager-connectivity-action-deprecated
+public class BroadcastNetwork extends BroadcastReceiver
+{
+    @Override
+    public void onReceive(Context context, Intent intent)
+    {
+
+    }
+}
